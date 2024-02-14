e2b1e1a9402ba01e5133fb3a8b3206b18853cbee
Joel Panepinto <joelpanepinto@macbook-pro.mynetworksettings.com>
2024-02-14 17:24:27 -0500

    arrow fct

---
+++ arrow_functions.js
@@ -0,0 +1,23 @@
+//// Task 1
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+}
+
+// functions as one-liner
+const double = arr => arr.map(val => val * 2);
+
+//// Task 2
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+}
+
+// with arrow functions 
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0)
